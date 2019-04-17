e9af7c26881235c9909b340a46787b633ec2a288
Antony Denyer <git@antonydenyer.co.uk>
2019-04-17 13:51:16 +0100

    Simple multinode example

--- example/keys.js
+++ example/keys.js
@@ -5,6 +5,9 @@ module.exports = {
     },
     node2: {
       publicKey: "Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs="
+    },
+    node3: {
+      publicKey: "k2zXEin4Ip/qBGlRkJejnGWdP9cjkK+DAvKNW31L2C8="
     }
   },
   pantheon: {
@@ -12,6 +15,16 @@ module.exports = {
       url: "http://localhost:20000",
       privateKey:
         "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"
+    },
+    node2: {
+      url: "http://localhost:20002",
+      privateKey:
+        "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"
+    },
+    node3: {
+      url: "http://localhost:20004",
+      privateKey:
+        "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"
     }
   }
 };
