e384e3a7b60efc62298f0ed2663105ee890986ee
ivaylo <iikirilov@gmail.com>
2019-05-17 10:50:20 +0100

    Test for privacy group id generation

---
+++ test/index.js
@@ -0,0 +1,19 @@
+const tape = require("tape");
+const Enclave = require("../src/index");
+const txFixtures = require("./keySets.json");
+
+tape("[EEA]: Privacy Group Generation", t => {
+  t.test("should generate correct privacy group id", st => {
+    /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
+    txFixtures.forEach(pg => {
+      const expected = pg.privacyGroupId;
+      const input = pg.privacyGroup;
+      const enclave = new Enclave({ _currentProvider: { host: "" } });
+      st.equal(
+        enclave.eea.generatePrivacyGroup({ privateFrom: input }),
+        Buffer.from(expected).toString("hex")
+      );
+    });
+    st.end();
+  });
+});
