a4610a8fdd9f8fc5698438887366e3773315e18f
cusden <ian.cusden@gmail.com>
2020-03-26 23:29:50 +0000

    Add support for priv_call JSON-RPC method
    Added web3.priv.privacyCall
    
    /**
       * Invokes a private contract function locally
       * @param options Options passed into `priv_call`
       * options map can contain the following:
       * privacyGroupId : Enclave id representing the receivers of the transactio
       * to : Contract address,
       * data : Encoded function call (signature + data)
       * blockNumber: Blocknumber  efaults to "latest"
       * @returns {Promise<AxiosResponse<T>>}
       */

---
+++ example/multiNodeExamplePrivateCall/deployContract.js
@@ -0,0 +1,51 @@
+const fs = require("fs")
+const path = require("path")
+
+const Web3 = require("web3")
+const EEAClient = require("../../src")
+
+const createGroup = require("../privacyGroupManagement/createPrivacyGroup")
+
+const { orion, besu } = require("../keys.js")
+
+const binary = fs.readFileSync(
+  path.join(__dirname, "../solidity/EventEmitter/EventEmitter.bin"),
+)
+
+const web3 = new EEAClient(new Web3(besu.node1.url), 2018)
+
+const createGroupId = () => {
+  return createGroup.createPrivacyGroup()
+}
+
+const createPrivateEmitterContract = privacyGroupId => {
+  const contractOptions = {
+    data: `0x${binary}`,
+    privateFrom: orion.node1.publicKey,
+    privacyGroupId,
+    privateKey: besu.node1.privateKey,
+  }
+  return web3.eea.sendRawTransaction(contractOptions)
+}
+
+const getPrivateContractAddress = transactionHash => {
+  console.log("Transaction Hash ", transactionHash)
+  return web3.priv
+    .getTransactionReceipt(transactionHash, orion.node1.publicKey)
+    .then(privateTransactionReceipt => {
+      console.log("Private Transaction Receipt\n", privateTransactionReceipt)
+      return privateTransactionReceipt.contractAddress
+    })
+}
+
+module.exports = async () => {
+  const privacyGroupId = await createGroupId()
+  const contractAddress = await createPrivateEmitterContract(privacyGroupId)
+    .then(getPrivateContractAddress)
+    .catch(console.error)
+  return { contractAddress, privacyGroupId }
+}
+
+if (require.main === module) {
+  module.exports()
+}
