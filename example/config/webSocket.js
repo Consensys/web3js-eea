13705cc467315a11ba35f40ded3ec520cf5d510a
Akua Nti <akua.nti@consensys.net>
2020-04-23 17:34:22 -0400

    Update example to show usage with JWT

--- example/config/webSocket.js
+++ example/config/webSocket.js
@@ -3,10 +3,16 @@ const EEAClient = require("../../src");
 
 // Define a WebSocket provider, passing in the desired options:
 // See https://github.com/ethereum/web3.js/tree/1.x/packages/web3-providers-ws for options
+
+// See https://besu.hyperledger.org/en/stable/HowTo/Interact/APIs/Authentication/#jwt-public-key-authentication
+// for details on authenticating with JWTs
+const JWT_TOKEN =
+  "ewogICJhbGciOiAibm9uZSIsCiAgInR5cCI6ICJKV1QiCn0.eyJpYXQiOjE1MTYyMzkwMjIsImV4cCI6NDcyOTM2MzIwMCwicGVybWlzc2lvbnMiOlsibmV0OnBlZXJDb3VudCJdfQ";
+
 const providerOptions = {
   timeout: 30000, // ms
   headers: {
-    authorization: "Basic username:password"
+    authorization: `Bearer ${JWT_TOKEN}`
   },
   reconnect: {
     auto: true,
