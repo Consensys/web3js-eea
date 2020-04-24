a9b9b1f6f627405e32ec30d20134a1c111ecd087
Akua Nti <akua.nti@consensys.net>
2020-04-24 17:31:36 -0400

    Explicitly pull the latest version of Besu
    CircleCI caches old versions of the hyperledger/besu:latest image
    and doesn't seem to update when new veersions are released. Pull
    the latest veeresion before running docker-compose.

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,9 @@
 NO_LOCK_REQUIRED=true
 . ./.env
 
+# Make sure we have an up-to-date image
+docker pull hyperledger/besu:${BESU_VERSION}
+
 # Build and run containers and network
 echo "${QUICKSTART_VERSION}" >> ${LOCK_FILE}
 echo "Starting network..."
