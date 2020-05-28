b96fcdf389de8993a4741201275715dbdfb07784
Nicolas MASSART <nicolas.massart@consensys.net>
2020-05-28 23:27:52 +0200

    review fixes

--- .circleci/organise_doc.js
+++ .circleci/organise_doc.js
@@ -1,39 +1,34 @@
-'use strict';
+'use strict'
 
-const fs = require('fs-extra');
-const args = require('minimist')(process.argv.slice(2));
+const fs = require('fs')
+const args = require('minimist')(process.argv.slice(2))
 
 // get SDK name and version from package files
 // they are used by JSDoc to generate the output directories
-const PACKAGE_JSON = require('../package.json');
-const PACKAGE_VERSION = PACKAGE_JSON.version;
-const PACKAGE_NAME = PACKAGE_JSON.name;
+const packageJSON = require('../package.json')
+const packageVersion = packageJSON.version
+const packageName = packageJSON.name
 
 // get the output directory
-const JSDOC_OUTPUT_DIR = require('../jsdoc.json').opts.destination;
+const jsdocJSON = require('../jsdoc.json')
+const jsdocOpts = jsdocJSON.opts
+const jsDocOutputDir = jsdocOpts.destination
 
 // root dir is the absolute path to use, provided by CI
-const ROOT_DIR = args.rootdir;
+const rootDir = args.rootdir
 // the tag being built and empty or null if none
-const TAG = args.tag;
+const tag = args.tag
 
 // if no tag, doc sill gon in latest directory
-const TARGET_DIR = (TAG == '' || TAG == null) ? 'latest' : TAG;
+const targetDir = (tag === '' || tag == null) ? 'latest' : tag
 
 // source directories where JSdoc put the doc
-const SOURCE_DIR_BASE = `${ROOT_DIR}/${JSDOC_OUTPUT_DIR}`;
-const SOURCE_DIR = `${SOURCE_DIR_BASE}/${PACKAGE_NAME}/${PACKAGE_VERSION}`;
+const sourceDirBase = `${rootDir}/${jsDocOutputDir}`
+const sourceDir = `${sourceDirBase}/${packageName}/${packageVersion}`
 
 // check if source exists and move files to latest or tag matching directory
-fs.pathExists(SOURCE_DIR)
-.then(exists => {
-  if(exists){
-    fs.move(SOURCE_DIR,`${ROOT_DIR}/docs/${TARGET_DIR}`)
-  }else{
-    throw(`${SOURCE_DIR} doesn't exist!`);
-  }
+fs.rename(sourceDir, `${rootDir}/docs/${targetDir}`, (err) => {
+  if (err) throw err
+  fs.rmdirSync(sourceDirBase, {recursive:true})
+  console.log('Doc organisation success')
 })
-// then remove the initial empty dir
-.then(() => {fs.remove(SOURCE_DIR_BASE)})
-.then(() => {console.log('Doc organisation success')})
-.catch(err => {console.error(err)});
