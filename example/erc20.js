dd242137a4ed57c476f916c0a98a5ebc8fd989ca
Sally MacFarlane <sally.macfarlane@consensys.net>
2020-07-24 13:09:01 +1000

    use keys.js values in erc20.js (#115)
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- example/erc20.js
+++ example/erc20.js
@@ -6,12 +6,13 @@ const EEAClient = require("../src");
 const HumanStandardTokenAbi = require("./solidity/HumanStandardToken/HumanStandardToken.json")
   .output.abi;
 const ethUtil = require("../src/custom-ethjs-util");
+const { orion, besu } = require("./keys.js");
 
 const binary = fs.readFileSync(
   path.join(__dirname, "./solidity/EventEmitter/EventEmitter.bin")
 );
 
-const web3 = new EEAClient(new Web3("http://localhost:20000"), 2018);
+const web3 = new EEAClient(new Web3(besu.node1.url), 2018);
 
 const contract = new web3.eth.Contract(HumanStandardTokenAbi);
 
@@ -31,19 +32,16 @@ const constructorArgs = web3.eth.abi
 
 const contractOptions = {
   data: `0x${binary}${constructorArgs}`,
-  privateFrom: "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo=",
-  privateFor: ["Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs="],
-  privateKey: "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"
+  privateFrom: orion.node1.publicKey,
+  privateFor: [orion.node1.publicKey],
+  privateKey: besu.node1.privateKey
 };
 
 web3.eea
   .sendRawTransaction(contractOptions)
   .then(hash => {
     console.log(`Transaction Hash ${hash}`);
-    return web3.priv.getTransactionReceipt(
-      hash,
-      "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo="
-    );
+    return web3.priv.getTransactionReceipt(hash, orion.node1.publicKey);
   })
   .then(privateTransactionReceipt => {
     console.log("Private Transaction Receipt");
@@ -61,12 +59,7 @@ web3.eea
       return element.name === "transfer";
     });
     const transferTo = `0x${ethUtil
-      .privateToAddress(
-        Buffer.from(
-          "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
-          "hex"
-        )
-      )
+      .privateToAddress(Buffer.from(besu.node2.privateKey, "hex"))
       .toString("hex")}`;
     const functionArgs = web3.eth.abi
       .encodeParameters(functionAbi.inputs, [transferTo, 1])
@@ -75,17 +68,16 @@ web3.eea
     return web3.eea.sendRawTransaction({
       to: contractAddress,
       data: functionAbi.signature + functionArgs,
-      privateFrom: "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo=",
-      privateFor: ["Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs="],
-      privateKey:
-        "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"
+      privateFrom: orion.node1.publicKey,
+      privateFor: [orion.node2.publicKey],
+      privateKey: besu.node1.privateKey
     });
   })
   .then(transactionHash => {
     console.log(`Transaction Hash ${transactionHash}`);
     return web3.priv.getTransactionReceipt(
       transactionHash,
-      "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo="
+      orion.node1.publicKey
     );
   })
   .then(privateTransactionReceipt => {
