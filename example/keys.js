a9e4313215ec051e11c617c383b72ee93818e342
Sally MacFarlane <sally.macfarlane@consensys.net>
2021-05-17 16:16:32 +1000

    use the right keys...
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- example/keys.js
+++ example/keys.js
@@ -1,51 +1,32 @@
 module.exports = {
-  chainID: "2018",
+  chainID: 2018,
   orion: {
     node1: {
-      publicKey: "GGilEkXLaQ9yhhtbpBT03Me9iYa7U/mWXxrJhnbl1XY=",
-      jwt:
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6WyIqOioiXSwicHJpdmFjeVB1YmxpY0tleSI6IkdHaWxFa1hMYVE5eWhodGJwQlQwM01lOWlZYTdVL21XWHhySmhuYmwxWFk9IiwiZXhwIjoxNjAwODk5OTk5MDAyfQ.pSMwiHSnVu3C1r8eH6OFnJIqWM4HG8n9OWbMHh0Qlw8j3k4RYp2BDBQjyS_qjSL2b2bSaHsEJ3y8EeTQK7Watitc3qs_KdS0If6z2Sn7-jGJjtqePAvl32zYtGRzFkbwI1DZGIkYfjjkc__1pKCwhJ_rQ9ggFuoh8AYzpz3F7aAGUFh9NMWl8WmT1bBjfyMgb7yUUV97UbfUOgI4CChrje0vWT5jGNyAweaNYrtHPxfXECySs99cMoqLBcsg6_ypUcrCXuONBZlI2e0bDLwpwrpfZj7YWLsxtPf2AxVN4JMGBwuuy5Rl4J3IHynm3JjgezPVS4UXKvUggb17XOGf5Q"
+      publicKey: "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo="
     },
     node2: {
-      publicKey: "KkOjNLmCI6r+mICrC6l+XuEDjFEzQllaMQMpWLl4y1s=",
-      jwt:
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6WyIqOioiXSwicHJpdmFjeVB1YmxpY0tleSI6IktrT2pOTG1DSTZyK21JQ3JDNmwrWHVFRGpGRXpRbGxhTVFNcFdMbDR5MXM9IiwiZXhwIjoxNjAwODk5OTk5MDAyfQ.XAUNQNuEnfmmoM0um-ikxe_aJv4vGALeD-dNy4_7pPDtPGrP5LmUZP6zcsm2KCmu9g2RhovxrCMAYDTCbSOV1T7sG7RBenNdw8AxD0kpRsz9IMzfpcaup4DIfm2aWnAnDsuND5wV9PRLLoomQqq_eEiI5s6fBi0aupYGE1wUB-mSwE6enaZD5Tkz_lTb_W4Gq-Z3lM57E-hwThUw_3RszL3HV-rl0n_hxZG0LQFEKmRnfbjPkt6Qm8RsyQ26O7K6AFbmEdv4N-1MQlzZWUACeb4JTOzEvlrz8E-Kdo1pkCrtr44veReN6Gmt0Cmv8Tv_1BKfggaKFinfaGUJJJFg_Q"
+      publicKey: "Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs="
     },
     node3: {
-      publicKey: "qaBVuA+nG7Yt+kru6CGI2VMxOBAK7b1KNmiJuInHtwc="
-    },
-    node11: {
-      publicKey: "JhOQwapQgv36ENsICu4K21YoeU+vFLvEU/Hnt/Fcoxc=",
-      jwt:
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6WyIqOioiXSwicHJpdmFjeVB1YmxpY0tleSI6IkpoT1F3YXBRZ3YzNkVOc0lDdTRLMjFZb2VVK3ZGTHZFVS9IbnQvRmNveGM9IiwiZXhwIjoxNjAwODk5OTk5MDAyfQ.C3eRrmo7pZqTaAtI3f_WdRVFrJQ0d0pk_RrwaiDmVNpOP3nuXp23OIFeDq6CDmS08mxQ00yThknLzaqd5ijsh8ozTjSSYDs6Ay5myI_ylEafaZHTwSbrBwXwr9J-i-K7szdyxUBjRPrQhMJoLBrm2pQ4aa6EwAqskRbRx9ocXjDnVSzK4tc7vSGeLDmv_LQm_A-OD4yvGx_kbScLPaZKff6yLlDKmTk4sYOURKkc-8tmTeOC3Q0aS3tuiBK-qJJMasPUSJ--kFa9cZqgLOTMv6o8Bl-F5bIoulABjRBqD64bx7Rpp2UWbrHId8SWqpoOlYL2endtYW6vqjd4usMvYg"
-    },
-    node12: {
-      publicKey: "dyKxmO5Ji7d8aZTXvk02x98l6oB9Q4MBTq6W4tIW+AM=",
-      jwt:
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6WyIqOioiXSwicHJpdmFjeVB1YmxpY0tleSI6ImR5S3htTzVKaTdkOGFaVFh2azAyeDk4bDZvQjlRNE1CVHE2VzR0SVcrQU09IiwiZXhwIjoxNjAwODk5OTk5MDAyfQ.psocMuOFSIIpiU6xFFLAvENGLDaTGc9nvGKQRz2OizT_sVZZowcewDWdOK5ZPDvaLSbweLNlnrDEycmNhLB0coGDf-gqK7pgeN_rMn4vMPFyBaeV3DoPnQzNl9JYrldPRzEv70Z6MInKy4mYm649Owow9K_MNuHTUjPdUZOypUVVRBae94B6PgQFrrWZnwZ3wjfZyc-e8cF8s_Ao067xjkoomBA-asYnPuMwTsyjdykypNx2Y0_cdjc8t-F1n2xWLqEvbx8QmrMNk9_2o9fURCSMd4QDq6dqswQOveTTTw2FbhicH9_dSmg_J64lFoLkg7BEDJ5yUIeZ2rF6ytv-wQ"
-    },
-    node21: {
-      publicKey: "rc2C5ldkby2W4PVgKRnKNx0Uw2pxjaKmq6ayZv1+8HU=",
-      jwt:
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6WyIqOioiXSwicHJpdmFjeVB1YmxpY0tleSI6IjlPRnpaNGdjdE4yUkcwQ0dMQSs3YU5zcGVUMmQzUGFlajhQbjZNaldBaGs9IiwiZXhwIjoxNjAwODk5OTk5MDAyfQ.GAGQI3gZM84oZ4hTQ6uiMyV5Z3hQHqoVy6bK5TqBrk-KASnKdB62EYkzwzLFLS43ZhnywbtQg5t8RlM0vZ4xbKmQXEi-jy-ls7PX0h7qGQupUeji-Y4nOmoVZ0P9Y3Eemy05baaKW0iAfZ7PPkJj8Wzt-H1K5rTQJCyCLjDf73XDnJWu3m_rrITJxVa76DM0zxE_aAdJaCIflBerhb7Rh5oLj3zmJyob-apzrgPxsV3wEHVoJBOzNQJOkXSd58cN2VXW4RVuzg3lvKGm3AbC7GoENSoDHsht8iu24tp8vn3IjnWDk6PxERZKfLwh6xTJSwWZs1qKYAN0lV1i95jzBA"
+      publicKey: "k2zXEin4Ip/qBGlRkJejnGWdP9cjkK+DAvKNW31L2C8="
     }
   },
   besu: {
     node1: {
-      url: "http://localhost:8545",
-      wsUrl: "ws://localhost:8546",
+      url: "http://localhost:20000",
+      wsUrl: "ws://localhost:20001",
       privateKey:
         "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"
     },
     node2: {
-      url: "http://localhost:8555",
-      wsUrl: "ws://localhost:8556",
+      url: "http://localhost:20002",
+      wsUrl: "ws://localhost:20003",
       privateKey:
         "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"
     },
     node3: {
-      url: "http://localhost:8565",
-      wsUrl: "ws://localhost:8566",
+      url: "http://localhost:20004",
+      wsUrl: "ws://localhost:20005",
       privateKey:
         "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"
     }
